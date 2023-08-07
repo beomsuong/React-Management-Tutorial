3d662a39143390466e7aedd3222a93f0851f2f2d
[Github <beomsuong0406@gmail.com>
2023-08-07 22:37:18 +0900

    Create Cumstomers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers =[{
+  'id':1,
+  'image': 'https://picsum.photos/64/64.jpg',
+  'name': '최범서',
+  'birthday':'994006',
+  'gender':'남자',
+  'job':'백수'
+},
+{
+  'id':2,
+  'image': 'https://picsum.photos/64/64.jpg',
+  'name': '이범서',
+  'birthday':'980406',
+  'gender':'남자',
+  'job':'개백수'
+},
+{
+  'id':3,
+  'image': 'https://picsum.photos/64/64.jpg',
+  'name': '삼범서',
+  'birthday':'00406',
+  'gender':'여자',
+  'job':'무백수'
+}
+
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2> Let's develop management system!</h2>
-    </div>
-  );
-}
+    <div>
+      {
+        customers.map(c=>{
+          return (
+            <Customer
+            keu={c.id}
+      id={c.id}
+      image={c.image}
+      name={c.name}
+      birthday={c.birthday}
+      gender={c.gender}
+      job={c.job}
+    />
+          );
+          
+        })
+      }
+      
+  
+</div>
+
+  )}
 
 export default App;
